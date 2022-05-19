cdc3838ce692889aa3dd6d58f0dd7b097bcc89ef
daria <n_daisy1@mail.ru>
2022-05-19 20:46:53 +0300

    something that works

--- AlDS_2c/GraphToHardLevel.h
+++ AlDS_2c/GraphToHardLevel.h
@@ -3,6 +3,8 @@
 #include <queue>
 #include <string>
 
+//TO DO: ADD ITERATORS FOR GRAPH
+
 #pragma once
 
 /*
@@ -21,7 +23,8 @@ private:
 
 public:
 	City() : _cityName("defaultName"), _countOfPeople(0) {};
-	City(std::string cityName, unsigned int countPeople) : _cityName(cityName), _countOfPeople(countPeople) {};
+	City(const std::string cityName, const  int countPeople) : _cityName(cityName),
+															   _countOfPeople(countPeople) {};
 	City(const City& city) {
 		_cityName = city._cityName;
 		_countOfPeople = city._countOfPeople;
@@ -87,8 +90,9 @@ private:
 
 public:
 	Road() : _weight(0), _priceRoad(0), _roadType(asphalt), _nameRoad("Samara") {};
-	Road(double weight, double pricaRoad, RoadType roadType, std::string nameRoad = "Samara") : _weight(weight),
-		_priceRoad(pricaRoad), _roadType(roadType), _nameRoad(nameRoad) {};
+	Road(const double weight, const double pricaRoad, const RoadType roadType, 
+							  const std::string nameRoad = "Samara") : _weight(weight),
+							  _priceRoad(pricaRoad), _roadType(roadType), _nameRoad(nameRoad) {};
 	Road(const Road& rhs) {
 		_weight = rhs._weight;
 		_priceRoad = rhs._priceRoad;
@@ -105,6 +109,10 @@ public:
 	void SetPriceRoad(const double priceRoad) { _priceRoad = priceRoad; }
 	void SetRoadType(const RoadType roadType) { _roadType = roadType; }
 	void SetNameRoad(const std::string nameRoad) { _nameRoad = nameRoad; }
+
+	double GetCost() const {
+		return _roadType * 10 + _priceRoad * 10 + _weight * 100;
+	}
 
 	Road& operator=(const Road& rhs) {
 		if (this == &rhs)
@@ -165,7 +173,8 @@ class Way {
 
 public:
 	Way() : _from(TVertex()), _where(TVertex()), _edge(TEdge()) {};
-	Way(TVertex from, TVertex whereVertex, TEdge edge) : _from(from), _where(whereVertex), _edge(edge) {};
+	Way(const TVertex from, const TVertex whereVertex, const TEdge edge) : _from(from), 
+														_where(whereVertex), _edge(edge) {};
 
 	TVertex GetFirstVertex() const { return _from; }
 	TVertex GetSecondVertex() const { return _where; }
@@ -228,7 +237,7 @@ struct std::equal_to<City>
 {
 	size_t operator()(const City& lhs, const City& rhs) const
 	{
-		return (lhs.GetCityName() == rhs.GetCityName()) && rhs.GetCountPeople() == rhs.GetCountPeople();
+		return lhs.GetCityName() == rhs.GetCityName() && rhs.GetCountPeople() == rhs.GetCountPeople();
 	}
 };
 template<>
@@ -242,22 +251,161 @@ struct std::equal_to<Road>
 };
 
 
-
-
 /*
 	summary
 
 	/summary
 */
 
-template <class TVertex = City, class TEdge = Road, class TEqual = std::equal_to<TVertex>>
+template <class TVertex = City, class TEdge = Road, class TEqual = std::equal_to<TVertex>, 
+													class TEqualEdge = std::equal_to<TEdge>>
 class Graph {
 private:
-	std::vector<Way<TVertex, TEdge>> _ways;
+	std::vector<TVertex> _vertices;
+	std::vector<Way<TVertex, TEdge>> _way;
+	size_t _size;
+
+	std::vector<std::vector<double>> _matr;
 
 public:
-	Graph() {};
+	Graph(): _size(0) {};
+	Graph(const std::vector<Way<TVertex, TEdge>> ways, const std::vector<TVertex> vertices, 
+		  const size_t size) : _way(ways), _size(size), _vertices(vertices) {};
+
+	Graph(const Graph& rhs) {
+		_way = rhs._way;
+		_vertices = rhs._vertices;
+		_size = rhs._size;
+	}
+
+	std::vector<Way<TVertex, TEdge>> GetWay() const { return _way; }
+	size_t GetSize() const { return _size; }
+
+	bool FindVertex(const TVertex& vertex) const {
+		/*
+			TO DO		add std::find adding iterators
+		*/
+		TEqual equal;
+		for (auto i = 0; i < _size; i++) {
+			if (equal(_vertices[i], vertex))
+				return true;
+		}
+		return false;
+	}
+	bool FindEdge(const Way<>& way) const {
+		TEqualEdge equal;
+		for (auto i = 0; i < _way.size(); i++) {
+			if (equal(way, _way[i]))
+				return true;
+		}
+
+		return false;
+	}
+
+	bool AddVertex(const TVertex& vertex) {
+		if (FindVertex(vertex))
+			return false;
+
+		_vertices.push_back(vertex);
+		_size++;
+		return true;
+	}
+
+	bool AddEdge(const Way<>& way) {
+		_way.push_back(way);
+		if (!FindVertex(way.GetFirstVertex()))
+			_vertices.push_back(way.GetFirstVertex());
+		if (!FindVertex(way.GetSecondVertex()))
+			_vertices.push_back(way.GetSecondVertex());
+		return true;
+	}
+	bool AddEdge(const TVertex& vertexFirst, const TVertex& vertexSecond) {
+		TEqual equal;
+		if (equal(vertexFirst, vertexSecond)) return false;
+		_way.push_back(Way<>(vertexFirst, vertexSecond, TEdge()));
+		if (!FindVertex(vertexFirst))
+			_vertices.push_back(vertexFirst);
+		if (!FindVertex(vertexSecond))
+			_vertices.push_back(vertexSecond);
+		return true;
+	}
+
+	//bool RemoveVertex(const TVertex& vertex) {
+	//	if (FindVertex(vertex))
+	//		return false;
+	//	
+	//}
+
+	//template<typename Selector>
+	//double Method(TEdge edge, Selector weightSelector) {
+	//	TEdge edge = …;
+	//	double weight = weightSelector(edge);
+	//	
+	//}
+
+	int GetIdByVertex(const TVertex& vertex) const {
+		TEqual equal;
+		for (auto i = 0; i < _size; i++) {
+			if (equal(_vertices[i], vertex))
+				return i;
+		}
+
+		return -1;
+	}
+
+	template<class Selector>
+	bool FillMatrix() {
+		_matr.resize(_size);		// Create zero matrix
+		for (int i = 0; i < _size; i++) {
+			_matr[i].resize(_size);
+			std::fill(_matr[i].begin(), _matr[i].end(), 0);
+		}
+
+		//double a = Selector(_way[0][0]);
+
+		for (auto i = 0; i < _way.size(); i++) {
+			int idFirstVertex = GetIdByVertex(_way[i].GetFirstVertex());
+			int idSecondVertex = GetIdByVertex(_way[i].GetSecondVertex());
+			_matr[idFirstVertex][idSecondVertex] = Selector(_way[i].GetEdge());
+		}
+
+		return true;
+	}
+
+	bool ImagineMatrix() const {
+		for (auto i = 0; i < _size; i++) {
+			for (auto j = 0; j < _size; j++) {
+				std::cout << _matr[i][j] << "	";
+			}
+			std::cout << "\n";
+		}
+	}
+
+
 };
 
+
+template<typename TEdge>
+struct Selector {
+	double operator()(const TEdge& edge) {
+		return static_cast<double>(edge);
+	}
+};
+template<>
+struct Selector<Road> {
+	double operator()(const Road& edge) {
+		return edge.GetCost();
+	}
+
+};
+
+
+
+//template<typename TWeightSelector>
+//void Method(…, TWeightSelector weightSelector) {
+//	TEdge edge = …;
+//	double weight = weightSelector(edge);
+//	…
+//}
 
 
